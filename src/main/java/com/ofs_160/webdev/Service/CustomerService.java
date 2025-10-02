4cdcf88fb86d36ca2bb853805a41d6ce783fb043
Daniel Nunez <143371163+nunez-daniel@users.noreply.github.com>
2025-10-01 20:37:34 -0700

    Update CustomerService.java

--- src/main/java/com/ofs_160/webdev/Service/CustomerService.java
+++ src/main/java/com/ofs_160/webdev/Service/CustomerService.java
@@ -3,9 +3,11 @@ package com.ofs_160.webdev.Service;
 import com.ofs_160.webdev.Model.Customer;
 import com.ofs_160.webdev.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @Service
 public class CustomerService {
@@ -13,6 +15,7 @@ public class CustomerService {
 
     @Autowired
     CustomerRepository customerRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(15);
 
     public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
@@ -34,4 +37,19 @@ public class CustomerService {
         customerRepository.save(customer);
     }
 
+    public Customer registerCustomer(Customer customer)
+    {
+        customer.setPasscode(bCryptPasswordEncoder.encode(customer.getPasscode()));
+        customer.setRole("CUSTOMER");
+        return customerRepository.save(customer);
+    }
+
+    public Customer findByEmail(String username) {
+        return customerRepository.findByEmail(username);
+    }
+
+    public Customer save(Customer customer) {
+        // repeated code for save, register, signup check for dupes
+        return customerRepository.save(customer);
+    }
 }
