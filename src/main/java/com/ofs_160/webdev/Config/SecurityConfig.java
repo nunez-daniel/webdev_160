f32b05a0ed21fc06df8667194c3c29b6db6eac29
Daniel Nunez <143371163+nunez-daniel@users.noreply.github.com>
2025-10-01 20:15:53 -0700

    Update SecurityConfig.java
    init commit

--- src/main/java/com/ofs_160/webdev/Config/SecurityConfig.java
+++ src/main/java/com/ofs_160/webdev/Config/SecurityConfig.java
@@ -1,21 +1,56 @@
 package com.ofs_160.webdev.Config;
 
 
-/*
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
-@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-        httpSecurity
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults());
-        return httpSecurity.build();
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(15)); // anyVal
+        return  provider;
+    }
+
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+            httpSecurity
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(req -> req
+
+
+
+                            .requestMatchers(HttpMethod.POST, "/product-manager-access")
+                            .hasAnyAuthority("ADMIN")
+                            .anyRequest().authenticated()
+                    )
+                    .formLogin(Customizer.withDefaults())
+                    .oauth2Login(Customizer.withDefaults());
+
+            return httpSecurity.build();
+
+    }
 
 }
 
-}
-
-*/
