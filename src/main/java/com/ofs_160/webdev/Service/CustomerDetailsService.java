82e150a91722675e51c7a5455302740600c3b916
Daniel Nunez <143371163+nunez-daniel@users.noreply.github.com>
2025-10-01 20:40:00 -0700

    Create CustomerDetailsService.java

---
+++ src/main/java/com/ofs_160/webdev/Service/CustomerDetailsService.java
@@ -0,0 +1,34 @@
+package com.ofs_160.webdev.Service;
+
+import com.ofs_160.webdev.Model.Customer;
+import com.ofs_160.webdev.Model.CustomerDetails;
+import com.ofs_160.webdev.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerDetailsService  implements UserDetailsService {
+
+    @Autowired
+    private CustomerRepository repo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+    {
+        // System.out.println("load User By Name Called");
+        Customer customer = repo.findByUsername(username);
+
+        if(customer == null)
+        {
+            // here would would redirect to register or confirm inputs is rights
+            System.out.println("404");
+            throw  new UsernameNotFoundException("404 not found in cd");
+        }
+
+        return new CustomerDetails(customer);
+    }
+}
