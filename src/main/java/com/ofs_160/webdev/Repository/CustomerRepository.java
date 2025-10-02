dc82af063d697a6278082ca2d0abf6d2b44649b6
Daniel Nunez <143371163+nunez-daniel@users.noreply.github.com>
2025-10-01 20:36:31 -0700

    Update CustomerRepository.java

--- src/main/java/com/ofs_160/webdev/Repository/CustomerRepository.java
+++ src/main/java/com/ofs_160/webdev/Repository/CustomerRepository.java
@@ -4,9 +4,8 @@ import com.ofs_160.webdev.Model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
+    Customer findByUsername(String username);
+    Customer findByEmail(String email);
 }
