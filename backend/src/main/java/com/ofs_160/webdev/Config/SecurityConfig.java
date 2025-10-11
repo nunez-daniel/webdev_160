c6219b9f262a390b74b7275fba6afb3a1fd8b61c
Daniel Nunez <daniel.nunez@sjsu.edu>
2025-10-11 16:23:29 -0700

    session handling

--- backend/src/main/java/com/ofs_160/webdev/Config/SecurityConfig.java
+++ backend/src/main/java/com/ofs_160/webdev/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ofs_160.webdev.Config;
 
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
 
 @Configuration
 @EnableMethodSecurity
@@ -24,6 +29,58 @@ public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    private String redirect_url = "http://localhost:5173/catalog";
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(req -> req
+
+                        .requestMatchers(HttpMethod.GET, "/products", "/products/{id}").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/signup").permitAll()
+                        .requestMatchers("/oauth2/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/product-manager-access")
+                        .hasAnyAuthority("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .successHandler((request, response, authentication) -> response.setStatus(HttpServletResponse.SC_OK)))
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(myAuthenticationSuccessHandler()));
+
+        // TODO.. Logout success handler
+
+        return httpSecurity.build();
+
+    }
+
+    // only do this on successful login
+    // else we can't redirect
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return new SimpleUrlAuthenticationSuccessHandler(redirect_url);
+    }
+
+    // https://docs.spring.io/spring-security/reference/servlet/integrations/cors.html#:~:text=%40Bean%0AUrlBasedCorsConfigurationSource%20corsConfigurationSource,configuration)%3B%0A%20%20%20%20return%20source%3B%0A%7D
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public AuthenticationProvider authenticationProvider()
@@ -32,30 +89,6 @@ public class SecurityConfig {
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder(15)); // anyVal
         return  provider;
-    }
-
-
-
-
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-            httpSecurity
-                    .csrf(csrf -> csrf.disable())
-                    .authorizeHttpRequests(req -> req
-
-                            .requestMatchers(HttpMethod.GET, "/products", "/products/{id}").permitAll()
-
-                            .requestMatchers(HttpMethod.GET, "/product", "/product/*").permitAll()
-                            .requestMatchers(HttpMethod.POST, "/product-manager-access")
-                            .hasAnyAuthority("ADMIN")
-                            .anyRequest().authenticated()
-                    )
-                    .formLogin(Customizer.withDefaults())
-                    .oauth2Login(Customizer.withDefaults());
-
-            return httpSecurity.build();
-
     }
 
 }
