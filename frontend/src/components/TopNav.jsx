c34628f544ef760011e2be2f8c4a1594d9e4e875
Agrika <agrika.gupta@sjsu.edu>
2025-10-22 16:47:28 -0700

    Changes to be committed: 	modified:   frontend/src/components/TopNav.jsx

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -138,12 +138,10 @@ export default function TopNav() {
               >
                 {recording ? (
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
                     fill="currentColor"
-                    class="bi bi-stop-btn"
-                    viewBox="0 0 16 16"
+                    className="h-4 w-4 absolute right-2 top-1/2 -translate-y-1/2"
+                    viewBox="0 0 19 19"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M6.5 5A1.5 1.5 0 0 0 5 6.5v3A1.5 1.5 0 0 0 6.5 11h3A1.5 1.5 0 0 0 11 9.5v-3A1.5 1.5 0 0 0 9.5 5z" />
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
