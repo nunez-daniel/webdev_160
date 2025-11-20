8237b456e7f2af451b98a67512d044760f2251f5
Agrika <agrika.gupta@sjsu.edu>
2025-11-19 18:14:25 -0800

    Changes to be committed: 	modified:   frontend/src/lib/use-toast.js

--- frontend/src/lib/use-toast.js
+++ frontend/src/lib/use-toast.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
-const TOAST_AUTO_DISMISS_DELAY = 6000
+const TOAST_AUTO_DISMISS_DELAY = 4000
 
 let count = 0
 
